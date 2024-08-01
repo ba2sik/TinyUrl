06d159073867d2b08371ad6d5a57f506c904b85a
Ron <ronzano123@gmail.com>
2024-08-01 16:56:26 +0300

    unique hash using md5

--- apps/backend/src/utils/shortener.ts
+++ apps/backend/src/utils/shortener.ts
@@ -1,5 +1,9 @@
-export const generateShortUrl = (url: string): string => {
-  const shortenedUrl = url;
+import { createHash } from 'crypto';
 
-  return shortenedUrl;
+export const generateShortUrl = (url: string): string => {
+  return generateUniqueId(url);
+};
+
+export const generateUniqueId = (str: string, length = 8): string => {
+  return createHash('md5').update(str).digest('hex').slice(0, length);
 };
